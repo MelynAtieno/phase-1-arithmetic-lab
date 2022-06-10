6ffd88d97b5e71b947b636ba9b76d1b29d775130
Melyn Atieno Were <melynatieno@gmail.com>
2022-06-10 16:34:51 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2
+print(multiply)
+
+let random = Math.floor(Math.random()*10)+1;
+print(random)
+
+let num3= 32
+let num4 = 7
+let mod = num3%num4
+print(mod)
+
+let set = (3,5,18,6,7,8,9,20)
+let max = Math.max(set)
+print(max)
+
 // Write your code here
